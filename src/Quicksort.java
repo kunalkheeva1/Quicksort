cf44be2ecf9e52ccf5ffb332a19c0d261b40aa84
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-11 19:51:19 -0700

    just generated a randonm index for pivot and then swap the element at that position with the last element for a good runtime

--- src/Quicksort.java
+++ src/Quicksort.java
@@ -1,19 +1,28 @@
 //One of the fastest algorithm so far, so do it carefully,,
 
+import java.util.Random;
+
 public class Quicksort {
     public static void quickSort(int [] arr, int lowIndex, int highIndex){
         //giving it a condition, which means it only has one value
         if(lowIndex>=highIndex){
             return;
         }
+        // some people choose pivot from end or front, and to make runtime smooth I will be taking a random pivot in between
+        // and then swap with the highindex
+
+        int pivotIndex = new Random().nextInt(highIndex-lowIndex) + lowIndex;
+        int pivot =arr[pivotIndex];
+        swap(arr, pivotIndex,highIndex);
+
+
+
 
         //here i will take a pivot position and then two pointer for the rest of the array
         //one on the right side of the pivot and another on the left side of it
-        int pivot = arr[highIndex];
+
         int leftPointer= lowIndex;
         int rightPointer = highIndex;
-
-
         //now traverse with the pointers wrt the pivot position
         while(leftPointer < rightPointer){
             // traversing the left side, and incrementing towards the rightPointer until the condition matches
@@ -27,12 +36,10 @@ public class Quicksort {
             //here i need a method to swap the elements so that all small will be shifted to left side
             swap(arr,leftPointer,rightPointer);
         }
-
         //meeting point of both the pointers
         swap(arr,leftPointer, highIndex);
 
         //since the first swap happened wrt to the conditions
-
         //lets make a recursive call, it is for left half first, as my first index will be same lowIndex,
 
         quickSort(arr, lowIndex,leftPointer-1);
@@ -45,7 +52,6 @@ public class Quicksort {
         int temp = arr[index1];
         arr[index1]= arr[index2];
         arr[index2]= temp;
-
     }
     //creating a method to print my array
     public static void printArr(int[]arr){
@@ -59,7 +65,6 @@ public class Quicksort {
     public static void quickSort(int [] arr){
         quickSort(arr,0,arr.length-1);
     }
-
     public static void main(String[] args) {
         //testing
         int[]arr= {3,4,2,0,84,4,9,6,7};
