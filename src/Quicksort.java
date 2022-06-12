2a6a5c1b53f600201db4eb0b0e2d19a943760a1f
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-11 18:48:23 -0700

    swap method has been created for its use in Quicksort

--- src/Quicksort.java
+++ src/Quicksort.java
@@ -1,43 +1,42 @@
+//One of the fastest algorithm so far, so do it carefully,,
+
 public class Quicksort {
-    public static int pivotIdx(int arr[], int low, int high){
-        int pivot = arr[high]; // took last element as the pivot
-        int i= low-1;     //this is to manage the new positions
-        for(int j=low; j<high; j++){
-            if(arr[j]<pivot){
-                i++;
-                //swap the current element and assign it ith position
-                int temp = arr[i];
-                arr[i]=arr[j];       //will provide jth value to i
-                arr[j]= temp;        //will provide ith value to j which is stored in temp
+    public static void quickSort(int [] arr, int lowIndex, int highIndex){
+        //here i will take a pivot position and then two pointer for the rest of the array
+        //one on the right side of the pivot and another on the left side of it
+
+    int pivot = arr[highIndex];
+    int leftPointer= lowIndex;
+    int rightPointer = highIndex;
+
+        //now traverse with the pointers wrt the pivot position
+        while(lowIndex<highIndex){
+            // traversing the left side, and incrementing towards the rightPointer until the condition matches
+            while(arr[leftPointer]<=arr[pivot] && lowIndex<highIndex){
+                lowIndex++;
             }
-        } i++;                  //after adding the space I wil find the space to adjust my pivot now
-        int temp = arr[i];       //this strategy when element is not smaller than the given one
-        arr[i]=pivot;           //basically just adding the pivot to the next position in between
-        arr[high]=temp;         //large element will acquire pivot position
-        return i;               //Now i wil have updated index of the pivot
-    }
-
-    //Creating a method to perform sorting and which will require the pivot position, and now dealing with the rest of the elements
-
-    public static void quickSort(int[]arr, int low, int high){
-        if(low<high){
-            int pivot = pivotIdx(arr,low, high);
-            //recurrsively calling the quicksort to get the remaining positions
-            quickSort(arr,low, pivot-1);
-            quickSort(arr,pivot+1, high);
+            //traversing the right side, and decrementing towards the leftPointer until the condition matches
+            while (arr[rightPointer]>=arr[pivot] && lowIndex<highIndex){
+                highIndex--;
+            }
+            //here i need a method to swap the elements
+            swap(arr,leftPointer,rightPointer);
         }
-    }
+        //now if the pointers are meeting at some level then just swap it with the high index which is pivot
+        swap(arr,leftPointer, highIndex);
 
+    }
+    //creating a swap method to swap the elements of an array.
+    public static void swap(int[] arr, int index1, int index2 ){
+        int temp = arr[index1];
+        for(int i=0; i<arr.length; i++){
+            arr[index1]= arr[index2];
+            arr[index2]= temp;
+        }
+
+    }
 
     public static void main(String[] args) {
-        int arr[]= {2,4,2,1,0,34,-1};
-        int n = arr.length;
-
-        quickSort(arr,0,n-1);
-        for(int i=0; i<n; i++){
-            System.out.print(arr[i]+ " ");
-        }
-        System.out.println();
 
     }
 }
